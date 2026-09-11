3ae16de04a6b3f3e30889152333e9593ef085ac2
ramox81 <184937705+ramox81@users.noreply.github.com>
2026-09-11 09:32:59 +0300

    Expose Edit Hatch in the Modify extension

---
+++ src/ui/ribbon/modify_tools/05_hatchedit.rs
@@ -0,0 +1,6 @@
+Tool {
+    command: "HATCHEDIT",
+    label: "Edit Hatch",
+    icon: include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/icons/modify_hatchedit.svg")),
+    options: &[],
+}
