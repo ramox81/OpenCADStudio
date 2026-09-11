313a2cc7fd830c0fe15694348ba80f4554b9236c
ramox81 <184937705+ramox81@users.noreply.github.com>
2026-09-11 09:33:02 +0300

    Expose Break at Point in the Modify extension

---
+++ src/ui/ribbon/modify_tools/08_breakatpoint.rs
@@ -0,0 +1,6 @@
+Tool {
+    command: "BREAKATPOINT",
+    label: "Break at Point",
+    icon: include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/icons/modify_breakatpoint.svg")),
+    options: &[],
+}
