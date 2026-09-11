b39b0dbafe0cc867989b687e39fa2d573ce77b3c
ramox81 <184937705+ramox81@users.noreply.github.com>
2026-09-11 09:32:58 +0300

    Expose Edit Spline in the Modify extension

---
+++ src/ui/ribbon/modify_tools/04_splinedit.rs
@@ -0,0 +1,6 @@
+Tool {
+    command: "SPLINEDIT",
+    label: "Edit Spline",
+    icon: include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/icons/modify_splinedit.svg")),
+    options: &[],
+}
